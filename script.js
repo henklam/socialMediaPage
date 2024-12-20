8437791c4c3962108353e185f48fc48e63f6e5d3
henklam <183881683+henklam@users.noreply.github.com>
2024-12-20 05:33:03 +0000

    done

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-async function get() {
+async function getComment(i) {
     let thing;
-    const resource = await fetch('https://jsonplaceholder.typicode.com/comments/1')
+    const resource = await fetch('https://jsonplaceholder.typicode.com/comments')
     .then(resource => resource.json())
     .then(json => thing = json);
-    console.log(thing);
+    return thing[i].body;
+}
+
+async function getUser(i) {
+    let thing;
+    const resource = await fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resource => resource.json())
+    .then(json => thing = json);
+    return thing[i].username;
+}
+
+async function getAddress(i) {
+    let thing;
+    const resource = await fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resource => resource.json())
+    .then(json => thing = json);
+    return thing[i].address;
+}
+
+async function getImage() {
+    const resource = await fetch('https://unsplash.it/1920/1080?random');
+    const blob = await resource.blob();
+    return blob;
+}
+
+async function loadUser(i) {
+    const image = await getImage(i);
+    document.getElementById("image" + i).src = URL.createObjectURL(image);
+    const username = await getUser(i);
+    document.getElementById("username" + i).innerHTML = "Username: " + username;
+    const comment = await getComment(i);
+    document.getElementById("comment" + i).innerHTML = "Comment: " + comment;
+    const address = await getAddress(i);
+    document.getElementById("address" + i).innerHTML = "Address: " + address.street + ", " + address.suite + ", " + address.city + ", " + address.zipcode + ", Latitude: " + address.geo.lat + ", Longitude: " + address.geo.lng;
 }
 
 
-get();
+for(let i = 1; i < 10; i++) {
+    loadUser(i);
+}
